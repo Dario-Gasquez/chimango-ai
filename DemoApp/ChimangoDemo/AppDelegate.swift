6e851c5097cb2353c7f4bd57596139431f84b38a
Dario Gasquez <dario.gasquez@gmail.com>
2021-10-10 20:46:03 -0300

    Update README to mention latest changes in usage instructions and demo app

--- DemoApp/ChimangoDemo/AppDelegate.swift
+++ DemoApp/ChimangoDemo/AppDelegate.swift
@@ -11,8 +11,6 @@ import ChimangoAI
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         // Chimango Visual Search Library setup
